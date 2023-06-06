f0ac8aaa0111bca85cebc1d3a69d979c55d64894
boydlm <114845124+boydlm@users.noreply.github.com>
2023-06-05 21:42:57 -0400

    created flask app (#2)

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Todo List in React
 
 ### Running the app
-### `npm start`
+### `npm run start`
+(if this does not work, make sure you are in the correct folder)
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
+
+### Running the flask app
+ ### `FLASK_APP=main.py flask run`
--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const fetchTasks = async () => {
+       let response = await fetch('http://localhost:5000/todo/getall'
+       );
+       if (response.ok) { // if HTTP-status is 200-299
+          // get the task lists
+          let json = await response.json();
+          setTodos(json);
+       } else {
+          alert("HTTP-Error: " + response.status);
+       }
+    }
+    fetchTasks();
+ }, [])
+
   return (
     <div className="App">
       <TodoForm addTodo={(todo) => {
