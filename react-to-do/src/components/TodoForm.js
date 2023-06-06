8ae946fa02f6a5ececcad8ec645c23193cd3a096
Leah Boyd <boydlm@outlook.com>
2023-06-05 21:39:58 -0400

    created flask app

--- react-to-do/src/components/TodoForm.js
+++ react-to-do/src/components/TodoForm.js
@@ -3,12 +3,27 @@ import React, { useState } from 'react';
 function TodoForm({ addTodo }) {
     const [userInput, setUserInput] = useState("");
 
-    const handleSubmit = e => {
+    async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+           method: 'POST',
+           headers: {
+              'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+        });
+        return response.json();
+     }
+  
+     const handleSubmit = async e => {
         e.preventDefault();
         if (!userInput) return;
-        addTodo({ complete: false, task: userInput, });
-        setUserInput("");
-    };
+        await postData('http://localhost:5000/todo/create',
+          { complete: false, task: userInput })
+          .then(data => {
+              addTodo(data);
+              setUserInput("");
+           })
+     };
 
     return (
         <form onSubmit={handleSubmit} className="todo-form">
---
+++ react-to-do/todo_service/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from flask import jsonify
+import json
+from flask_cors import CORS #include this line
+app = Flask(__name__)
+app.run(debug=True)
+CORS(app)
+# read file
+with open('tasks.json', 'r') as myfile:
+    data=myfile.read()
+# parse file
+obj = json.loads(data)
+@app.route('/todo/getall',methods=['GET'])
+def getTasks():
+    return jsonify(obj)
+@app.route('/todo/create',methods=['POST'])
+def createTask():
+    req_data = request.get_json()
+    obj.append(req_data)
+    return jsonify(req_data)
+@app.route('/todo/update',methods=['PUT'])
+def updateTask():
+    req_data = request.get_json()
+    for idx, task in enumerate(obj):
+        if task.get('task') == req_data["task"]:
+            obj.pop(idx)
+            obj.insert(idx,req_data)
+            break
+    return jsonify(req_data)
+@app.route('/todo/delete',methods=['DELETE'])
+def deleteTask():
+    req_data = request.get_json()
+    for idx, task in enumerate(obj):
+        if task.get('task') == req_data["task"]:
+            obj.pop(idx)
+            return jsonify(req_data)
+    return "Item Not Found"
